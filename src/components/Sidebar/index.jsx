dfc7581d90ad69a423f1a3e0a2cdaf33daba0d79
William <william.jose.wjd@gmail.com>
2022-06-08 09:29:20 -0300

    ajustes

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './styles.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,16 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/williamwjd.png" />
                 <strong>William Dias</strong>
                 <span>Dev Java</span>
             </div>
 
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     )
