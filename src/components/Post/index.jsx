64e887cac782aaac7c0d3d54a2d30636b4201609
William <wildias@brasilseg.com.br>
2022-06-09 09:41:47 -0300

    criando e estilizando componente POST

---
+++ src/components/Post/index.jsx
@@ -0,0 +1,31 @@
+import styles from './styles.module.css';
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/diego3g.png" alt="" />
+                    <div className={styles.authorInfo}>
+                        <strong>William Dias</strong>
+                        <span>Backend Developer Java</span>
+                    </div>
+                </div>
+
+                <time title="09 de junho de 2022 ás 09:15" dateTime="2022-06-11 09:15:00">Públicado há 1h</time>
+            </header>
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>
+                    Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+                </p>
+                <p>👉{' '} <a href="#">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketseat</a>
+                </p>
+            </div>
+        </article>
+    )
+}
